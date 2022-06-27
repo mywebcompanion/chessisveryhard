570071387b8a76153f081eccfa30afb54a88ec31
Aditya Jadhav <adityaj2003@gmail.com>
2022-06-28 05:05:59 +0530

    Stockfish code taken from stockfish.js

---
+++ stockfish.js/src/engineOnBoard.js
@@ -0,0 +1,58 @@
+var engine = typeof STOCKFISH === "function" ? STOCKFISH() : new Worker('../stockfish.js/src/stockfish.js');
+var evaler = typeof STOCKFISH === "function" ? STOCKFISH() : new Worker('../stockfish.js/src/stockfish.js');
+
+var prevLine = null;
+
+engine.onmessage = function(event) {
+    var line;
+    if (event && typeof event === "object") {
+        
+        line = event.data;
+    } else {
+        line = event;
+    }
+    
+    console.log("Reply engine: " + line);
+    if (line.includes("bestmove")) {
+        showBestMove(line);
+        evalCurrentPosition(prevLine);
+    }
+    
+    if (prevLine == null) {
+        prevLine = line;
+    }
+    else {
+    prevLine = line;
+    }
+    
+}
+
+
+evaler.onmessage = function(event) {
+    var line;
+    if (event && typeof event === "object") {
+        
+        line = event.data;
+    } else {
+        line = event;
+    }
+    
+    if (line.includes("bestmove")) {
+        showBestMove(line);
+    }
+    
+    else if(line.includes("Final evaluation")) {
+        updateEvalDiv(line);
+    }
+    
+}
+
+
+console.log("message sent");
+engine.postMessage("uci");
+engine.postMessage("isready");
+evaler.postMessage("uci");
+evaler.postMessage("isready");
+
+
+
